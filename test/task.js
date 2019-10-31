dde78ec948a4b1a214f44e6bd098a2b5b50db56b
ngryman <ngryman@gmail.com>
2019-10-31 12:44:22 +0100

    :arrow_up: bump all dependencies to the latest version

--- test/task.js
+++ test/task.js
@@ -9,121 +9,152 @@ import chokidar from 'chokidar'
 import fs from 'fs'
 
 test.cb('bundle a file', t => {
-  vfs.src('test/fixtures/a+b.js')
+  vfs
+    .src('test/fixtures/a+b.js')
     .pipe(bro())
     .pipe(assert.length(1))
-    .pipe(assert.first(
-      d => t.deepEqual(
-        d.contents.toString().match(/exports = '[ab]'/g),
-        ["exports = 'a'", "exports = 'b'"]
+    .pipe(
+      assert.first(d =>
+        t.deepEqual(d.contents.toString().match(/exports = '[ab]'/g), [
+          "exports = 'a'",
+          "exports = 'b'"
+        ])
       )
-    ))
+    )
     .pipe(assert.end(t.end))
 })
 
 test.cb('bundle a stream', t => {
-  vfs.src('test/fixtures/a+b.js')
+  vfs
+    .src('test/fixtures/a+b.js')
     .pipe(bro())
     .pipe(assert.length(1))
-    .pipe(assert.first(
-      d => t.deepEqual(
-        d.contents.toString().match(/exports = '[ab]'/g),
-        ["exports = 'a'", "exports = 'b'"]
+    .pipe(
+      assert.first(d =>
+        t.deepEqual(d.contents.toString().match(/exports = '[ab]'/g), [
+          "exports = 'a'",
+          "exports = 'b'"
+        ])
       )
-    ))
+    )
     .pipe(assert.end(t.end))
 })
 
 test.cb('bundle multiple files separately', t => {
-  vfs.src(['test/fixtures/a+b.js', 'test/fixtures/a+c.js'])
+  vfs
+    .src(['test/fixtures/a+b.js', 'test/fixtures/a+c.js'])
     .pipe(bro())
     .pipe(assert.length(2))
-    .pipe(assert.first(
-      d => t.deepEqual(
-        d.contents.toString().match(/exports = '[ab]'/g),
-        ["exports = 'a'", "exports = 'b'"]
+    .pipe(
+      assert.first(d =>
+        t.deepEqual(d.contents.toString().match(/exports = '[ab]'/g), [
+          "exports = 'a'",
+          "exports = 'b'"
+        ])
       )
-    ))
-    .pipe(assert.second(
-      d => t.deepEqual(
-        d.contents.toString().match(/exports = '[ac]'/g),
-        ["exports = 'a'", "exports = 'c'"]
+    )
+    .pipe(
+      assert.second(d =>
+        t.deepEqual(d.contents.toString().match(/exports = '[ac]'/g), [
+          "exports = 'a'",
+          "exports = 'c'"
+        ])
       )
-    ))
+    )
     .pipe(assert.end(t.end))
 })
 
 test.cb('bundle an empty file', t => {
-  vfs.src('test/fixtures/empty.js')
+  vfs
+    .src('test/fixtures/empty.js')
     .pipe(bro())
     .pipe(assert.length(1))
-    .pipe(assert.first(
-      d => t.true(d.contents.toString().length > 0 /* browserify runtime */)
-    ))
+    .pipe(
+      assert.first(d =>
+        t.true(d.contents.toString().length > 0 /* browserify runtime */)
+      )
+    )
     .pipe(assert.end(t.end))
 })
 
 test.cb('use incremental build', t => {
   let times = []
-  vfs.src('test/fixtures/incremental.js')
+  vfs
+    .src('test/fixtures/incremental.js')
     .pipe(bro())
     .on('time', time => times.push(time))
-    .pipe(assert.end(() => {
-      vfs.src('test/fixtures/incremental.js')
-        .pipe(bro())
-        .on('time', time => times.push(time))
-        .pipe(assert.end(() => {
-          t.true(times[1] < times[0])
-          t.end()
-        }))
-    }))
+    .pipe(
+      assert.end(() => {
+        vfs
+          .src('test/fixtures/incremental.js')
+          .pipe(bro())
+          .on('time', time => times.push(time))
+          .pipe(
+            assert.end(() => {
+              t.true(times[1] < times[0])
+              t.end()
+            })
+          )
+      })
+    )
 })
 
 test.cb('accept browserify transforms', t => {
-  vfs.src('test/fixtures/es6.js')
-    .pipe(bro({
-      transform: babelify.configure({ presets: ['es2015'] })
-    }))
+  vfs
+    .src('test/fixtures/es6.js')
+    .pipe(
+      bro({
+        transform: babelify.configure({ presets: ['@babel/preset-env'] })
+      })
+    )
     .pipe(assert.length(1))
-    .pipe(assert.first(
-      d => t.is(d.contents.toString().match(/_classCallCheck/).length, 1)
-    ))
+    .pipe(
+      assert.first(d =>
+        t.is(d.contents.toString().match(/_classCallCheck/).length, 1)
+      )
+    )
     .pipe(assert.end(t.end))
 })
 
 test.cb('log a syntax error', t => {
   const restore = catchStdout()
 
-  vfs.src('test/fixtures/syntax_error.js')
-    .pipe(bro(() => {
+  vfs.src('test/fixtures/syntax_error.js').pipe(
+    bro(() => {
       t.truthy(~restore().indexOf('SyntaxError'))
       t.end()
-    }))
+    })
+  )
 })
 
 test.cb('emit a syntax error when asked to', t => {
-  vfs.src('test/fixtures/syntax_error.js')
+  vfs
+    .src('test/fixtures/syntax_error.js')
     .pipe(bro({ error: 'emit' }))
     .on('error', err => t.is(err.name, 'SyntaxError'))
     .pipe(assert.end(t.end))
 })
 
 test.cb('call an error handler when provided', t => {
-  vfs.src('test/fixtures/syntax_error.js')
+  vfs
+    .src('test/fixtures/syntax_error.js')
     .pipe(bro({ error: err => t.is(err.name, 'SyntaxError') }))
     .pipe(assert.end(t.end))
 })
 
 test.cb('bundle a stream when deeply nested, #5', t => {
-  vfs.src('test/fixtures/modules/d/e/f.js')
+  vfs
+    .src('test/fixtures/modules/d/e/f.js')
     .pipe(bro())
     .pipe(assert.length(1))
-    .pipe(assert.first(
-      d => t.deepEqual(
-        d.contents.toString().match(/exports = '[ab]'/g),
-        ["exports = 'a'", "exports = 'b'"]
+    .pipe(
+      assert.first(d =>
+        t.deepEqual(d.contents.toString().match(/exports = '[ab]'/g), [
+          "exports = 'a'",
+          "exports = 'b'"
+        ])
       )
-    ))
+    )
     .pipe(assert.end(t.end))
 })
 
@@ -136,35 +167,41 @@ test.cb('gulp.watch detect changes in main entry, #4', t => {
   chokidar.watch('test/fixtures/watch_entry.js').on('change', bundle)
 
   function bundle() {
-    vfs.src('test/fixtures/watch_entry.js')
+    vfs
+      .src('test/fixtures/watch_entry.js')
       .pipe(bro())
-      .pipe(assert.first(
-        d => {
+      .pipe(
+        assert.first(d => {
           if (1 === calls) {
             t.is(d.contents.toString().match(/alert\("yay"\)/).length, 1)
           }
-        }
-      ))
-      .pipe(assert.end(() => {
-        if (2 === ++calls) {
-          fs.unlinkSync('test/fixtures/watch_entry.js')
-          return t.end()
-        }
+        })
+      )
+      .pipe(
+        assert.end(() => {
+          if (2 === ++calls) {
+            fs.unlinkSync('test/fixtures/watch_entry.js')
+            return t.end()
+          }
 
-        // mtime resolution can be 1-2sec depending on the os
-        setTimeout(() => {
-          fs.appendFileSync('test/fixtures/watch_entry.js', 'alert("yay")')
-        }, 100)
-      }))
+          // mtime resolution can be 1-2sec depending on the os
+          setTimeout(() => {
+            fs.appendFileSync('test/fixtures/watch_entry.js', 'alert("yay")')
+          }, 100)
+        })
+      )
   }
 })
 
 test.cb('accept an external option, #25', t => {
-  vfs.src('test/fixtures/external.js')
+  vfs
+    .src('test/fixtures/external.js')
     .pipe(bro({ external: './modules/b' }))
     .pipe(assert.length(1))
-    .pipe(assert.first(
-      d => t.is(d.contents.toString().match(/exports = '[ab]'/g).length, 1)
-    ))
+    .pipe(
+      assert.first(d =>
+        t.is(d.contents.toString().match(/exports = '[ab]'/g).length, 1)
+      )
+    )
     .pipe(assert.end(t.end))
 })
